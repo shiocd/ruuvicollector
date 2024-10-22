8d1a1a9d3c35d1403de9b979d2dfa61431d3fef9
shiocd <ta@iki.fi>
2024-10-22 14:32:59 +0300

    Make backup script work in ramdisk and process one archive at a time

--- utils/backup.sh
+++ utils/backup.sh
@@ -3,19 +3,29 @@
 # set up ssh pubkey login for TARGET host
 # note: this can be insecure if your device ever gets compromised
 # crontab example:
-# @daily TARGET=user@foo.bar:backup/ ~/backup.sh >> backup.log
+# TARGET=user@foo.bar:backup/
+# @daily ~/ruuvicollector/backup.sh >> ~/ruuvi_backup.log
 
 if [ -z "${TARGET}" ]; then
   echo "Backup TARGET env unset"
   exit
 fi
 
+# work in ramdisk to reduce sd writes
+cd /tmp && sudo rm -rf ruuvi_backup
+
 # take backup of all data
 influxd backup -portable ruuvi_backup
 
 # recompress archives (>50% smaller with xz)
-gunzip ruuvi_backup/*tar.gz
-nice xz ruuvi_backup/*tar
+pushd ruuvi_backup
+for arc_in in *gz
+do
+  gunzip $arc_in
+  tarball=$(basename $arc_in .gz)
+  nice xz $tarball
+done
+popd
 
 # dump some sd and fs stats
 sudo ~/sdmon/src/sdmon /dev/mmcblk0 >ruuvi_backup/sd.txt
